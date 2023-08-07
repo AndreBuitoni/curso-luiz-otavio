3764d0eca687b804ca34c070dcfaae4e22aa0bd1
André Buitoni <a.buitoni@gmail.com>
2023-08-07 19:46:49 -0300

    Separated another component from APP.js - Posts component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { Component} from 'react';
 import './App.css';
 
 // Minhas importações
-import { PostCard } from './components/PostCard';
 import { loadPosts } from './utils/load-posts'
+import { Posts } from './components/Posts';
 
 class App extends Component {
   state = {
@@ -28,17 +28,7 @@ class App extends Component {
     return(
 
       <section className='container'>
-        <div className="posts">
-          {posts.map(post => (
-            <PostCard 
-              key={post.id} 
-              title={post.title} 
-              body={post.body} 
-              id={post.id} 
-              cover={post.cover}
-            />
-          ))}
-        </div>
+        <Posts posts={posts} />
       </section>
     )
   }
