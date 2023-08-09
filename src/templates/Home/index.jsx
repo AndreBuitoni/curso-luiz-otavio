d25fdb0e0121a952ec2cbebf087b0372ec6658af
André Buitoni <a.buitoni@gmail.com>
2023-08-08 22:47:24 -0300

    Moved TextInput element to a new component
    - and adjusted it's CSS

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -8,6 +8,7 @@ import './styles.css';
 import { loadPosts } from '../../utils/load-posts'
 import { Posts } from '../../components/Posts';
 import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
 
 export class Home extends Component {
   state = {
@@ -63,24 +64,19 @@ export class Home extends Component {
     return(
 
       <section className='container'>
-        {!!searchValue && (
-          <>
-            <h1>Search Value: {searchValue}</h1><br /><br />
-          </>
-        )}
-
-        <input 
-          onChange={this.handleChange}
-          value={searchValue} 
-          type="search" 
-        /><br /><br /><br />
+        <div className="search-container">
+          {!!searchValue && (
+            <h1>Search Value: {searchValue}</h1>
+          )}
+          <TextInput searchValue={searchValue} handleChange={this.handleChange} />
+        </div>
 
         {filteredPosts.length > 0 && (
           <Posts posts={filteredPosts} />
         )}
 
         {filteredPosts.length === 0 && (
-          <p>Não existem posts com esse termo</p>
+          <p>Não existem posts com esse</p>
         )}
         
 
