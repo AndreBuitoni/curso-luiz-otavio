ddc1536f0811c3e5905d3651b42c9f38a358f5f9
André Buitoni <a.buitoni@gmail.com>
2023-08-18 16:40:01 -0300

    Created test and snapshot of PostCard component

---
+++ src/components/PostCard/PostCard.spec.jsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react'
+import { PostCard } from '.'
+
+const props = {
+  title: 'title 1',
+  body: 'body 1',
+  id: 1,
+  cover: 'img/img.png'
+}
+
+describe('<PostCard />', () => {
+  it('should render PostCard correctly', () => {
+    render(<PostCard {...props} />)
+
+    expect(screen.getByRole('img', { name: /TITLE 1/i })).toHaveAttribute('src', 'img/img.png')
+    expect(screen.getByRole('heading', { name: 'title 1 1' })).toBeInTheDocument()
+    expect(screen.getByText('body 1')).toBeInTheDocument()
+  })
+
+  it('should match snapshot', () => {
+    const { container } = render(<PostCard {...props} />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
